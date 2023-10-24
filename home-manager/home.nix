1a1b4330a6f0e1d058bc8a88bd07a7121b1ef531
Andreas Taenzer <andreast.work@gmail.com>
2023-10-24 09:03:23 -0400

    changed

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -42,7 +42,7 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-    home.packages = with pkgs; [ firefox vscode wofi img mpv zip unzip cava nerdfonts iosevka ];
+    home.packages = with pkgs; [ firefox vscode wofi img mpv zip unzip cava nerdfonts iosevka dunst ];
 
   # Enable home-manager and git
     programs.home-manager.enable = true;
