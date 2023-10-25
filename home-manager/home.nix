829461bb7e9c8cba12fd954a32badb32f3f596a1
Andreas Taenzer <andreast.work@gmail.com>
2023-10-25 10:31:15 -0400

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,7 +9,7 @@
   imports = [
     # If you want to use home-manager modules from other flakes (such as nix-colors):
     # nix-colors.homeManagerModules.default
-
+    ./wayland
    
   ];
 
---
+++ home-manager/wayland/default.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, lib, ... }: {
+  imports = [
+    
+    #./dunst.nix
+    #./rofi.nix
+    #./utilities.nix
+    
+  ];
+
+  home.packages = with.pkgs [
+    swaybg
+    swaylock
+    swayidle
+    
+  ];
+
+  # make some environment tweaks for wayland
+  home.sessionVariables = {
+    GDK_BACKEND = "wayland,x11";
+    # some nixpkgs modules have wrapers
+    # that force electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+    # make qt apps expect wayland
+    QT_QPA_PLATFORM = "wayland";
+    # set backend for sdl
+    SDL_VIDEODRIVER = "wayland";
+    # fix modals from being attached on tiling wms
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    # fix java gui antialiasing
+    _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
+    # firefox and mozilla software expect wayland
+    MOZ_ENABLE_WAYLAND = "1";
+  };
+}
