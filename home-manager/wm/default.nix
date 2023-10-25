819a6f3001c44c22895419473f528d0b73f1858f
Andreas Taenzer <andreast.work@gmail.com>
2023-10-25 10:56:40 -0400

    sway

--- home-manager/wm/default.nix
+++ home-manager/wm/default.nix
@@ -1,7 +1,8 @@
 { config, pkgs, ... }: {
 
 imports = [
-    ./hyprland
+    #./hyprland
+    ./sway.nix
 ];
 }
 
---
+++ home-manager/wm/sway.nix
@@ -0,0 +1,17 @@
+{ inputs, lib, config, pkgs, ... }: {
+
+wayland.windowManager.sway = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4";
+      # Use kitty as default terminal
+      terminal = "kitty"; 
+      startup = [
+        # Launch Firefox on start
+        {command = "firefox";}
+      ];
+    };
+  };
+
+
+}
