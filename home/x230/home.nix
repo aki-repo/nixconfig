7c5a01e672b93985f36e22990643ae224f8b9e9b
Andreas Taenzer <andreast.work@gmail.com>
2023-10-30 11:46:32 -0400

    u

--- home/x230/home.nix
+++ home/x230/home.nix
@@ -44,7 +44,7 @@
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
   
-  home.packages = with pkgs; [ zip unzip iosevka font-awesome ];
+  home.packages = with pkgs; [ zip unzip iosevka nerdfonts font-awesome ];
 
   # Enable home-manager and git
     programs.home-manager.enable = true;
