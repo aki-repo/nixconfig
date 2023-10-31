183b243c41009774efc68220b3795ca80692105e
Andreas Taenzer <andreast.work@gmail.com>
2023-10-31 08:41:11 -0400

    u

--- hosts/x230/configuration.nix
+++ hosts/x230/configuration.nix
@@ -88,7 +88,7 @@ nixpkgs = {
 
     # This will additionally add your inputs to the system's legacy channels
     # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+    # nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
     settings = {
       substituters = ["https://hyprland.cachix.org"];
